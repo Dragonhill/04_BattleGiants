7cb0406c09de21d40d391e841985dda5c7b590d4
IPSOFT\BDunham <bret.dunham@ipsoft.com>
2019-05-28 11:01:29 -0400

    ai controller fixed

--- BattleBeast/Source/BattleBeast/TankAIController.cpp
+++ BattleBeast/Source/BattleBeast/TankAIController.cpp
@@ -3,42 +3,41 @@
 #include "TankAIController.h"
 #include "CoreMinimal.h"
 #include "Engine/World.h"
-//#include "GameFramework/Actor.h"
 #include "GameFramework/PlayerController.h"
 #include "BattleBeast.h"
 
-
-ATank* ATankAIController::GetPlayerTank() const
-{
-	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
-	if (!PlayerPawn) { return nullptr; } // Note the !, very important
-	return Cast<ATank>(PlayerPawn);
-}
 
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
+	//auto ControlledTank = GetControlledTank();
+	//if (!ControlledTank)
 	auto PlayerTank = GetPlayerTank();
-	/*
+	
 	if (!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AIController not possesing a tank"));
+		//UE_LOG(LogTemp, Warning, TEXT("AIController not possesing a tank"));
 		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *(ControlledTank->GetName()));
+		//UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *(ControlledTank->GetName()));
 		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *(PlayerTank->GetName()));
 	}
-	*/
+	
 }
 
 
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; } // Note the !, very important
+	return Cast<ATank>(PlayerPawn);
 }
