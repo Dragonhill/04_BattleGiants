b15d943b9c02d58aa022c174722debe4a6fd239f
IPSOFT\BDunham <bret.dunham@ipsoft.com>
2019-06-03 11:14:47 -0400

    hit location now works

--- BattleBeast/Source/BattleBeast/TankPlayerController.cpp
+++ BattleBeast/Source/BattleBeast/TankPlayerController.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "CoreMinimal.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "BattleBeast.h"
 
 
@@ -57,7 +60,8 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector & HitLocation) const
 	FVector LookDirection;
 	if (GetLookDirection(ScreenLocation, LookDirection))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *LookDirection.ToString());
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *LookDirection.ToString());
+		GetLookVectorHitLocation(LookDirection, HitLocation);
 	}
 
 	// Line-trace along that LookDirection, and see what we hit (up to max range)
@@ -75,4 +79,25 @@ bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector &
 		CameraWorldLocation,
 		LookDirection
 	);
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector & HitLocation) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
+
+	if (GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		StartLocation,
+		EndLocation,
+		ECollisionChannel::ECC_Visibility)
+		)
+	{
+		HitLocation = HitResult.Location;
+		return true;
+	}
+
+	HitLocation = FVector(0);
+	return false; // Line trace didn't succeed
 }
