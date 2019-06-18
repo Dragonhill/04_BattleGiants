d7a507753a55e97f7a15c1b3fbaa1859f1fce76d
IPSOFT\BDunham <bret.dunham@ipsoft.com>
2019-06-18 09:37:56 -0400

    fixed gravity bug?

--- BattleBeast/Source/BattleBeast/TankBarrel.h
+++ BattleBeast/Source/BattleBeast/TankBarrel.h
@@ -9,7 +9,7 @@
 /**
  * 
  */
-UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+UCLASS(meta = (BlueprintSpawnableComponent))//, hidecategories = ("Collision"))
 class BATTLEBEAST_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
--- BattleBeast/Source/BattleBeast/TankTurret.h
+++ BattleBeast/Source/BattleBeast/TankTurret.h
@@ -9,7 +9,7 @@
 /**
  * 
  */
-UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+UCLASS(meta = (BlueprintSpawnableComponent))//, hidecategories = ("Collision"))
 class BATTLEBEAST_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
